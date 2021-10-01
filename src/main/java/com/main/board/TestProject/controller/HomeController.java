e47f1a46550cfb5f56cab5c43b4f21a64849be00
종호 윤 <ho2106@naver.com>
2021-10-01 20:50:53 +0900

    최초 구성파일 커밋 해당 커밋에는 Test차 사용했던 컨트롤러와 DB Connection이 포함되어 있으며, 삭제를 안한 부분임.

---
+++ src/main/java/com/main/board/TestProject/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.main.board.TestProject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/log-in")
+    public String LogIn(){
+
+        return "thymeleaf/login";
+    }
+}
